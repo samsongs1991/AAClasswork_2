751b47e93460112be6585ec43fc581dfb504f16b
Samuel Song <samsongs@umich.edu>
2021-07-20 17:39:57 -0600

    Added case in steps reducer to remove steps if its todo is destroyed

--- W11D2/todos/frontend/reducers/steps_reducer.js
+++ W11D2/todos/frontend/reducers/steps_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_STEPS, RECEIVE_STEP, REMOVE_STEP } from "../actions/step_actions";
+import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO } from "../actions/todo_actions";
 
 const initialState = {
     1: { // this is the step with id = 1
@@ -34,6 +35,18 @@ const stepsReducer = (state = initialState, action) => {
       case REMOVE_STEP:
         delete nextState[action.step.id];
         return nextState;
+
+      // -------------------------------------------------------------
+      // *** Below, the case will remove steps of a todo if the todo is deleted
+      // case REMOVE_TODO:
+      //   for(let step in nextState) {
+      //     if(nextState[step].todo_id === action.todo.id) {
+      //       delete nextState[step]
+      //     }
+      //   }
+      //   return nextState;
+      // -------------------------------------------------------------
+
       default:
         return state;
     }
